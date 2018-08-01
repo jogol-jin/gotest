c6877c4c3dda6ef4b63f1e98fc2d4d25fe87b85b
castiel.jin <castiel.jin@klook.com>
2018-08-01 20:12:26 +0800

    访问百度

--- src/main.go
+++ src/main.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"github.com/Unknwon/log"
+	"io/ioutil"
+)
 
 func main() {
 	fmt.Println("this is a git test!")
 	fmt.Println("add in dev-test4!")
 	fmt.Println("add in dev-test3!")
+	client := http.Client{}
+
+	resp, err := client.Get("http://www.baidu.com")
+	if err != nil {
+		log.Info("get error, err:%v", err)
+		return
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Info("read body error, err:%v", err)
+		return
+	}
+	fmt.Println(string(b))
 }
